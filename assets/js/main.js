e28b02c12396a9cec963a8e041f962bfccc27ba3
Andrea Bellome <andrea.bellome@gmail.com>
2023-04-18 17:34:15 +0200

    created hello vue

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+      return {
+        /* variables Vue 3 */
+        message: 'Hello Vue!'
+      }
+    },
+    methods: {
+        /* functions Vue 3 */
+        
+    }
+  }).mount('#app')
